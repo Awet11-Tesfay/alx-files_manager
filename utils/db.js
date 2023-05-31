2416dc99d15adffc5a476b7e44ce0b03673fd272
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-31 17:01:12 +0300

    mongodb_utils

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,17 @@
 import { MongoClient } from 'mongodb';
+
 const Host = process.env.DB_HOST || 'localhost';
 const Port = process.env.DB_PORT || 27017;
 const Database = process.env.DB_DATABASE || 'files_manager';
 const Urls = `mongodb://${Host}:${Port}`;
 
-
+/**
+ * DbClient class
+ */
 class DBClient {
+    /**
+     * Constructor that creates a client to mongodb
+     */
   constructor() {
     MongoClient.connect(Urls, { useUnifiedTopology: true }, (error, client) => {
       if (!error) {
@@ -19,15 +25,24 @@ class DBClient {
     });
   }
 
+  /**
+   * Return true when the connection to redis is success
+   */
   isAlive() {
     return Boolean(this.db);
   }
 
+  /**
+   * Returns the number of documents
+   */
   async nbUsers() {
     const Users = this.usersCollection.countDocuments();
     return Users;
   }
 
+  /**
+   * Return the number of documents in collection files
+   */
   async nbFiles() {
     const Files = this.filesCollection.countDocuments();
     return Files;
