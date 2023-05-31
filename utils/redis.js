f523aa4cc03bde99f1becdc3a95a2400fc0cd3d6
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-31 13:16:22 +0300

    redis

--- utils/redis.js
+++ utils/redis.js
@@ -1,38 +1,36 @@
-import { promisify } from 'utl';
+import { promisify } from 'util';
 import { createClient } from 'redis';
-
-const redis = require('redis')
 
 
 class RedisClient {
-    constructor() {
-        this.client = createClient();
-        this.isClientConnected = True;
-        this.client.on('error', (err) => {
-            console.error('Redis client failed to connect:', err.message || err.toString());
-            this.isClientConnected = false;
-        });
-        this.client.on('connect', () => {
-            this.isClientConnected = true;
-        });
-    }
+  constructor() {
+    this.client = createClient();
+    this.isClientConnected = true;
+    this.client.on('error', (err) => {
+      console.error('Redis client failed to connect:', err.message || err.toString());
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
+  }
 
-    isAlive() {
-        return this.isClientConnected;
-    }
+  isAlive() {
+    return this.isClientConnected;
+  }
 
-    async get(key) {
-        return promisify(this.client.GET).bind(this.client)(key);
-    }
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
+  }
 
-    async set(key, val, time_duration) {
-        await promisify(this.client.SETEX)
-          .bind(this.client)(key, time_duration, val);
-    }
+  async set(key, value, duration) {
+    await promisify(this.client.SETEX)
+      .bind(this.client)(key, duration, value);
+  }
 
-    async del(key) {
-        await promisify(this.client.DEL).bind(this.client)();
-    }
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
 }
 
 export const redisClient = new RedisClient();
